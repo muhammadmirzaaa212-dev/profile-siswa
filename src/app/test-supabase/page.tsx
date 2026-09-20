f4de5be7c5f85990bdd64f92726e177e4fc8636f
Muhammad Mirza <muhammadmirzaaa212@gmail.com>
2026-09-20 19:08:11 +0700

    feat: test apakah data Supabase telah connect

---
+++ src/app/test-supabase/page.tsx
@@ -0,0 +1,10 @@
+import { supabase } from "../../../lib/supabase";
+
+export default async function TestSupabasePage() {
+  const { data, error } = await supabase.from('projects').select('*');
+
+  console.log('Data dari Supabase:', data);
+  console.log('Error (jika ada):', error);
+
+  return <p>Cek terminal VS Code kalian untuk melihat hasilnya.</p>;
+}
